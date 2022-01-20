25356bb6c222534937c3fbbf91824e6ae24cfee4
Lipin <lipinnambiar@gmail.com>
2022-01-20 15:18:11 +0530

    removed fracty

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -123,9 +123,7 @@ class RecipeView extends View {
       <svg class="recipe__icon">
         <use href="${icons}.svg#icon-check"></use>
       </svg>
-      <div class="recipe__quantity">${
-        ing.quantity ? fracty(ing.quantity).toString() : ''
-      }</div>
+      <div class="recipe__quantity">${ing.quantity}</div>
       <div class="recipe__description">
         <span class="recipe__unit">${ing.unit}</span>
         ${ing.description}
